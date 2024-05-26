b4a200883fab6cbf1a176acd314d24c9fe811860
dominicmena <mena.dominic@gmail.com>
2024-05-26 18:53:01 -0500

    added layouts folder in views and created app.blade.php file in layouts. copied over header from welcome.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
                     <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
 
-                            <p>aslkfjeoifjsae</p>
+                            <p class="red">aslkfjeoifjsae</p>
                             <a
                                 href="https://laravel.com/docs"
                                 id="docs-card"
